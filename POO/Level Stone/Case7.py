55ff24b23145cacb6395e68fcdb88e7bcefa882f
Alex Lopez <alexxxd23@gmail.com>
2022-07-22 10:26:02 -0300

    Case7 terminado

--- POO/Level Stone/Case7.py
+++ POO/Level Stone/Case7.py
@@ -1,6 +1,3 @@
-from textwrap import fill
-
-
 class Producto:
     def __init__(self, codigoBarra, cantidadLitros, precio, marca):
         self.codigoBarra = codigoBarra
@@ -25,8 +22,7 @@ class Producto:
         self.marca = marca
     
     def __str__(self):
-        return self.getCodigoBarra()
-    
+        return self.getCodigoBarra()  
 class AguaMineral(Producto):
     def __init__(self, codigoBarra, cantidadLitros, precio, marca, origen):
         super().__init__(codigoBarra, cantidadLitros, precio, marca)
@@ -39,10 +35,7 @@ class AguaMineral(Producto):
     
     def __str__(self):
         return f"Tipo de producto: {__class__.__name__}\nCodigo de barra: {self.getCodigoBarra()}\nCantidad de litros: {self.getCantidadLitros()}\n"\
-            f"Precio: {self.getPrecio()}\nMarca: {self.getMarca()}\nOrigen: {self.getOrigen()}\n-----------------------------------------------------\n"
-           
-            
-        
+            f"Precio: {self.getPrecio()}\nMarca: {self.getMarca()}\nOrigen: {self.getOrigen()}\n-----------------------------------------------------\n"        
 class Gaseosa(Producto):
     def __init__(self, codigoBarra, cantidadLitros, precio, marca, porcentajeAzucar, promocion):
         super().__init__(codigoBarra, cantidadLitros, precio, marca)
@@ -61,10 +54,7 @@ class Gaseosa(Producto):
     
     def __str__(self):
         return f"Tipo de producto: {__class__.__name__}\nCodigo de barra: {self.getCodigoBarra()}\nCantidad de litros:{self.getCantidadLitros()}"\
-            f"Precio: {self.getPrecio()}\nMarca: {self.getMarca()}\nPorcentaje de azucar: {self.getPorcentAzucar}\nPromocion: {self.getPromocion()}\
-            -----------------------------------------------------"
-
-
+            f"\nPrecio: {self.getPrecio()}\nMarca: {self.getMarca()}\nPorcentaje de azucar: {self.getPorcentAzucar}\nPromocion: {self.getPromocion()} "
 class Deposito:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -73,6 +63,10 @@ class Deposito:
     #Nombre del almacen
     def getNombre(self):
         return self.nombre
+
+    def lengthAlmacen(self):
+        return len(self.almacen)
+
     #Agregar productos
     def addProduct(self, product):
         if len(self.almacen) == 0:
@@ -83,66 +77,70 @@ class Deposito:
                 if product.getCodigoBarra() != i.getCodigoBarra():
                     self.almacen.append(product)
                     print("Producto agregado")
-            else:
-                print("Ocurrio un error, no se puede agregar un producto al almacen debido a que tiene el mismo codigo de barra.")
+                else:
+                    print("Ocurrio un error, no se puede agregar un producto al almacen debido a que tiene el mismo codigo de barra.")
+        
     #Eliminar productos        
     def removeProduct(self, CodigoBarra):
-        for product in self.almacen:
-            if CodigoBarra == product.getCodigoBarra():
-                self.almacen.remove(product)
-                print("Producto eliminado.")
-            else:
-                print("No se pudo eliminar el producto, debido a que no se encontraron resultados.")
+        if self.lengthAlmacen() != 0:
+            for product in self.almacen:
+                if CodigoBarra == product.getCodigoBarra():
+                    self.almacen.remove(product)
+                    print("Producto eliminado.")
+                else:
+                    print("No se pudo eliminar el producto, debido a que no se encontraron resultados.")
+        else:
+            print("No se puede eliminar productos, ya que el almacen no contiene productos.")
     
     #Mostrar informacion
     def showAlmacen(self):
-        print("Productos actualmente almacenados: ")
-        for products in self.almacen:
-            print(products)
+        if len(self.almacen) != 0:
+            print("-----Productos actualmente almacenados----")
+            for products in self.almacen:
+                print(products)
+        else:
+            print("No hay productos almacenados actualmente.")
 
     #Calcula el precio de todos los productos
     def totalPriceAllProducts(self):
-        total= 0
-        for product in self.almacen:
-            total+= product.getPrecio()
-        return total
+            total= 0 
+            for product in self.almacen:
+                total+= product.getPrecio()
+            return total
     
     #Calcula el precio total de una marca de bebida.
     def TotalPriceMarcaProducts(self, marca):
-        total=0
-        for product in self.almacen:
-            if marca == product.getMarca():
-                total+=product.getPrecio()
-        return total
-    
+            total=0
+            for product in self.almacen:
+                if marca == product.getMarca():
+                    total+=product.getPrecio()
+            return total
+
     #Info del deposito.
     def __str__(self):
         return self.getNombre()
-    
 # ------------------------------------------------------------------------- #
 def menuAndDecision():
     try:
         decision = int(input("¿Que desea realizar?\n1.Ver productos almacenados\n2.Agregar productos\n3.Eliminar productos"\
-        f"\n4.Calcular precio de bebidas almacenadas\n5.Calcular precio de una marca en especifico.\nDecision: "))
+        f"\n4.Calcular precio de bebidas almacenadas\n5.Calcular precio de una marca en especifico.\n6.Salir\nDecision: "))
     except:
         print("ERROR: Usted debe ingresar una opcion correcta.(Solo se permite numeros, no caracteres.)\n")
     else:
         return decision
-
 def promocion(strPromocion):
     if strPromocion.lower() == 'si':
         return True
     else: 
         return False
-
-def fillProduct():
+def fillProduct(deposito):
     follow = True
     while(follow):
-        tipo = input("¿Que tipo de producto desea registrar en el almacen?\n1.Agua mineral\n2.Gaseosa")
+        tipo = input("¿Que tipo de producto desea registrar en el almacen?\n1.Agua mineral\n2.Gaseosa\nRespuesta: ")
         if tipo == '1':
             try:
                 codigoBarra= int(input("Ingrese por favor el codigo de barra del producto: "))
-                cantLitros = int(input("Ingrese por favor la cantidad de litros de la bebida: "))
+                cantLitros = float(input("Ingrese por favor la cantidad de litros de la bebida: "))
                 precio = float(input("Ingrese el precio del producto: $"))
                 marca = input("Ingrese la marca del producto: ")
                 origen= input("Ingrese el origen del producto(Ciudad, Manantial...etc): ")
@@ -153,7 +151,7 @@ def fillProduct():
         elif tipo == '2':
             try:
                 codigoBarra= int(input("Ingrese por favor el codigo de barra del producto: "))
-                cantLitros = int(input("Ingrese por favor la cantidad de litros de la bebida: "))
+                cantLitros = float(input("Ingrese por favor la cantidad de litros de la bebida: "))
                 precio = float(input("Ingrese el precio del producto: $"))
                 marca = input("Ingrese la marca del producto: ")
                 porcentajeAzucar= float(input("Ingrese el porcentaje de azucar del producto: %"))
@@ -167,24 +165,57 @@ def fillProduct():
         decision= input("¿Desea volver a intentarlo?(Escriba 'si', si es que desea reintentarlo. Cualquier tecla para no): ")
         if decision.lower() != 'si':
             follow = False
-
+def findAndRemove(deposito):
+    codigoBarra= int(input("Ingrese por favor el codigo de barra del producto a eliminar: "))
+    deposito.removeProduct(codigoBarra)
+def calculateAll(deposito):
+    if deposito.lengthAlmacen() != 0:
+        total = deposito.totalPriceAllProducts()
+        print(f"El precio total de todos los productos es de: ${total}")
+    else:
+        print("No se puede calcular el precio total, debido a que el almacen no contiene productos.")
+def calculateSpecificProduct(deposito):
+    if deposito.lengthAlmacen() != 0:
+        marca = input("Ingrese por favor la marca del producto: ")
+        total = deposito.TotalPriceMarcaProducts(marca)
+        if total != 0:
+            print(f"El precio total de todos los productos con la marca '{marca}' es de: ${total}")
+        else:
+            print("ERROR: No se encontraron productos, con esa marca.")
+    else:
+        print("El almacen no contiene productos.")
 
 #MAIN
-follow = True
+decision = 0
 nombreAlmacen = input("Ingrese el nombre del almacen: ")
 deposito = Deposito(nombreAlmacen)
 
-while(follow):
+while(decision != 6):
     print(f"-----BIENVENIDO AL ALMACEN {deposito}-----")
-    if menuAndDecision() == 1:
+    decision = menuAndDecision()
+    if decision == 1:
         deposito.showAlmacen()
-    elif menuAndDecision() == 2:
-        fillProduct()
+    elif decision == 2:
+        fillProduct(deposito)
+    elif decision == 3:
+        findAndRemove(deposito)
+    elif decision == 4:
+        calculateAll(deposito)
+    elif decision == 5:
+        calculateSpecificProduct(deposito)
+    elif decision == 6:
+        print("Este programa finalizara... Gracias por ejecutarlo y probarlo.")
+    else:
+        print("ERROR: Opcion incorrecta.")
 
-    decision = input("¿Desea volver a intentarlo?(Escriba 'si' para reintentar. De lo contrario cualquier tecla): ")
-    if decision.lower() != 'si':
-        follow = False
-print("Que tenga buenas tardes.")
+
+    
+
+
+ 
+
+
+
     
 
         
